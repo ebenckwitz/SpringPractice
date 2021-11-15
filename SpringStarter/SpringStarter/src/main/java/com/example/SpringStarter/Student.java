b333728cf970fbbd3621e3706e5440c0d4a7d81a
Ethan Benckwitz <60863350+ebenckwitz@users.noreply.github.com>
2021-11-15 12:18:51 -0700

    Able to get address in table of DB

--- SpringStarter/SpringStarter/src/main/java/com/example/SpringStarter/Student.java
+++ SpringStarter/SpringStarter/src/main/java/com/example/SpringStarter/Student.java
@@ -2,13 +2,13 @@ package com.example.SpringStarter;
 
 import javax.persistence.*;
 
-@Data
+//@Data
 @Entity
-@Builder
+//@Builder
 @Table(name="new_student",
 uniqueConstraints=@UniqueConstraint(
 		name="emailed_unique",
-		columnNames="emailId"
+		columnNames= "emailId"
 		)
 		)
 
@@ -16,21 +16,23 @@ public class Student {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	
-	private Long studentId;
+	private int studentId;
 	private String firstName;
 	private String lastName;	
 	//@Column(name="emailed_address")
 	private String emailId;
+	private String address;
 	
-	public Student(String fname, String lname, String emailID) {
+	public Student(String fname, String lname, String emailID, String addy) {
 		this.firstName = fname;
 		this.lastName = lname;
 		this.emailId = emailID;
+		this.address = addy;
 	}
 	
 	@Override
 	public String toString() {
-		return "Student [emailId=" + emailId + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+		return "Student [emailId=" + emailId + ", firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + "]";
 	}
 	
 }
